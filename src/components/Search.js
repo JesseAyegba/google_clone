defc50de32d7ec4169047fd266a01cd2994d2eb8
Jesse Ayegba <jessejoseph64@gmail.com>
2021-03-19 02:44:46 +0100

    Search Icons added to search bar

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,12 +1,15 @@
 import React, { useState } from "react";
 import "./Search.css";
+import google from "../svgs/Google.svg";
+import { AiOutlineSearch } from "react-icons/ai";
+import { BsFillMicFill } from "react-icons/bs";
 
 function Search() {
   const [search, setSearch] = useState({
     query: "",
   });
   let handleSubmit = (e) => {
-    e.preventDefault();
+    // e.preventDefault();
   };
   let handleChange = (e) => {
     let text = e.target.value;
@@ -16,14 +19,25 @@ function Search() {
   };
   return (
     <div className="search">
-        <h1>{search.query}</h1>
-      <form onSubmit={(e) => handleSubmit(e)} className="search__form" autoComplete="off">
-        <input
-          name="query"
-          onChange={(e) => handleChange(e)}
-          required
-          type="text"
-        />
+      <div className="search__imageWrapper">
+        <img className="search__image" src={google} alt="Google Logo" />
+      </div>
+      <form
+        onSubmit={(e) => handleSubmit(e)}
+        className="search__form"
+        autoComplete="off"
+      >
+        <div className="search__box">
+          <AiOutlineSearch className="search__icon" />
+          <input
+            name="query"
+            onChange={(e) => handleChange(e)}
+            required
+            type="text"
+            className="search__input"
+          />
+          <BsFillMicFill className="search__icon search__icon--mic" />
+        </div>
         <button type="submit">Search</button>
       </form>
     </div>
